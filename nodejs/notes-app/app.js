c76ab18b573aa4e6973b909fff0312536bca6d49
Essankov <essank99@gmail.com>
2019-07-12 22:38:26 -0500

    add more code

--- nodejs/notes-app/app.js
+++ nodejs/notes-app/app.js
@@ -1,8 +1,44 @@
 const getNotes = require('./notes.js')
 const color = require('chalk')
+const yargs = require('yargs')
 
-const msg = getNotes()
-console.log(msg)
+yargs.version('1.1.0')
+
+//create add command
+yargs.command(
+    {
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function(){
+        console.log('Adding a new note!')}
+    })
+
+//create remove command
+yargs.command(
+    {
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function(){
+        console.log('Removing a note!')}
+    })
+
+//create list command
+yargs.command(
+    {
+    command: 'list',
+    describe: 'List your note',
+    handler: function(){
+        console.log('Listing out all notes!')}
+    })
+
+//create read command
+yargs.command(
+    {
+    command: 'read',
+    describe: 'Read a note',
+    handler: function(){
+        console.log('Reading a note!')}
+    })
 
 
-console.log(color.green("Success!"))
+console.log(yargs.argv)
