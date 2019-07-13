7e11bcd1c43f0cb5dca81f1676dd8ea13715eaa2
Essankov <essank99@gmail.com>
2019-07-13 02:11:21 -0500

    add stuff

---
+++ nodejs/notes-app/app.js
@@ -0,0 +1,58 @@
+const getNotes = require('./notes.js')
+const color = require('chalk')
+const yargs = require('yargs')
+
+yargs.version('1.1.0')
+
+//create add command
+yargs.command(
+    {
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv){
+        console.log('Title: ' + argv.title)
+        console.log('Body: ' + argv.body)
+    }   
+    })
+
+//create remove command
+yargs.command(
+    {
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function(){
+        console.log('Removing a note!')}
+    })
+
+//create list command
+yargs.command(
+    {
+    command: 'list',
+    describe: 'List your note',
+    handler: function(){
+        console.log('Listing out all notes!')}
+    })
+
+//create read command
+yargs.command(
+    {
+    command: 'read',
+    describe: 'Read a note',
+    handler: function(){
+        console.log('Reading a note!')}
+    })
+
+
+yargs.parse()
