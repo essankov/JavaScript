a51b855f5e6136c337b3845a2262b3ac8cf62450
Essankov <essank99@gmail.com>
2019-07-25 19:26:38 -0500

    add method js

---
+++ methods.js
@@ -0,0 +1,22 @@
+name = ['Essa', 'Nabeel', 'Issa', 'Alkhamees']
+
+//delete first name
+//name.shift()
+//console.log(name)
+
+//add first name
+//name.unshift('something')
+//console.log(name)
+
+//delete last name
+//name.pop()
+//console.log(name)
+
+//add last name
+//name.push('somehting')
+//console.log(name)
+
+//change in middle
+//2- where to start 1- how many last parameter what to change
+name.splice(2, 1, 'something')
+console.log(name)
