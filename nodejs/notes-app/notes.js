fa6b33f61236cb281ae01b261506df1789515190
Essankov <essank99@gmail.com>
2019-08-01 01:55:03 -0500

    reading notes

--- nodejs/notes-app/notes.js
+++ nodejs/notes-app/notes.js
@@ -6,12 +6,26 @@ const getNotes = () =>
     return "Your notes..."
 }
 
+
+const readNote = (title) => {
+    const notes = loadNotes()
+    const note = notes.find((note) => note.title === title)
+
+    if (note) {
+        console.log(chalk.inverse(note.title))
+        console.log(note.body)
+    } else {
+        console.log(chalk.red.inverse('Note not found!'))
+    }
+}
+
 const listNotes = () => {
 
     const notes = loadNotes()
     console.log(chalk.inverse('Your notes'))
 
     notes.forEach((note) => {
+        const duplicateNote = notes.find((note) => note.title === title)
         console.log(note.title)
     });
 }
@@ -36,10 +50,10 @@ const removeNote = (title) =>
 const addNotes = (title, body) =>
 {
     const notes = loadNotes()
-    const duplicateNotes = notes.filter((note) => note.title === title)
+    const duplicateNote = notes.find((note) => note.title === title)
         
     
-    if(duplicateNotes.length === 0)
+    if(!duplicateNote)
     {
         notes.push({
             title: title,
@@ -75,4 +89,5 @@ module.exports = {
     getNotes: getNotes,
     addNotes: addNotes,
     removeNote: removeNote,
+    readNote: readNote,
 }
