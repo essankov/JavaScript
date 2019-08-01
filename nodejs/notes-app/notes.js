d75cb17c27b0e995d25a812b0a1f2b964d0e951e
Essankov <essank99@gmail.com>
2019-08-01 02:20:31 -0500

    how to debug txt being add

--- nodejs/notes-app/notes.js
+++ nodejs/notes-app/notes.js
@@ -6,6 +6,8 @@ const readNote = (title) => {
     const notes = loadNotes()
     const note = notes.find((note) => note.title === title)
 
+    
+
     if (note) {
         console.log(chalk.inverse(note.title))
         console.log(note.body)
