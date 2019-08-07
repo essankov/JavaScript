948e66029a19d0247f215cf7c8aaff5d661d8960
Essankov <essank99@gmail.com>
2019-08-07 05:24:09 -0500

    change the weather app code

--- nodejs/weather-app/app.js
+++ nodejs/weather-app/app.js
@@ -1,16 +1,28 @@
-const request = require('request')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 
 
-geocode('Boston', (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-})
+const address = process.argv[2]
 
+if (!address) {
+    console.log('Please provide an address')
+} else {
+    geocode(address, (error, data) => {
+    
+        if (error) {
+           return console.log(error)
+        } 
+    
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+    
+            if (error) {
+                return console.log(error)
+            }
+    
+            console.log(data.location)
+            console.log(forecastData)
+          })
+    })
+}
 
-forecast(-75.7088, 44.1545, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-  })
