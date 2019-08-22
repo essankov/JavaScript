66435eae3dd9cc1f0782e0fb839f93d1bd8ae2eb
Essankov <essank99@gmail.com>
2019-08-22 01:09:25 -0500

    delete code

--- nodejs/web-server/public/js/app.js
+++ nodejs/web-server/public/js/app.js
@@ -1,5 +1,3 @@
-console.log("¯\\_(ツ)_/¯")
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
