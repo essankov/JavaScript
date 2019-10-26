a13e1dc41e5138bf2965400bbb720b420dfb0757
Essankov <essank99@gmail.com>
2019-10-25 21:00:54 -0500

    create auth.js

--- nodejs/task-manager/src/index.js
+++ nodejs/task-manager/src/index.js
@@ -8,6 +8,18 @@ const jwt = require('jsonwebtoken')
 const app = express()
 const port = process.env.PORT || 3000
 
+// app.use((req, res, next) => {
+//     if (req.method === 'GET') {
+//         res.send('GET requests are disabled')
+//     } else {
+//         next()
+//     }
+// })
+
+// app.use((req, res, next) => {
+//     res.status(503).send('Come Back Later')
+// })
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
--- nodejs/task-manager/src/routers/user.js
+++ nodejs/task-manager/src/routers/user.js
@@ -1,27 +1,23 @@
 const express = require('express')
-const router = new express.Router()
 const User = require('../models/user')
+const auth = require('../middleware/auth')
+const router = new express.Router()
 
 // signup 
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
 
     try {
-        const token = await user.generateAuthToken()
         await user.save()
+        const token = await user.generateAuthToken()
         res.status(201).send({ user, token })
     } catch (e) {
         res.status(400).send(e)
     }
 })
 
-router.get('/users', async (req, res) => {
-    try {
-        const users = await User.find()
-        res.send(users)
-    } catch (e) {
-        res.status(500).send()
-    }
+router.get('/users/me', auth, async (req, res) => {
+    res.send(req.user)
 })
 
 // login
