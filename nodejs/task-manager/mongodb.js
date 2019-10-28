d512750f822ad9ac655461f3cdaeb601ccc8831d
Essankov <essank99@gmail.com>
2019-10-28 18:12:33 -0500

    add commint

--- nodejs/task-manager/mongodb.js
+++ nodejs/task-manager/mongodb.js
@@ -1,8 +1,9 @@
 //CRUD create read update delete
+//essankov
 
 const { MongoClient, ObjectID } = require('mongodb')
 
-const connectionURL = 'mongodb://127.0.0.1:27017'
+const connectionURL =  'mongodb+srv://essank:Essank1234@@cluster0-3tbqx.mongodb.net/test?retryWrites=true&w=majority' //'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
