c986ab950eb03a95b8279c319febb76937dc167d
Essankov <essank99@gmail.com>
2019-11-01 20:41:33 -0500

    User/Task Relationship

--- nodejs/task-manager/src/index.js
+++ nodejs/task-manager/src/index.js
@@ -27,3 +27,19 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 }) 
+
+
+const Task = require('./models/task')
+const User = require('./models/user')
+
+const main = async () => {
+    // const task = await Task.findById('5dbcd60af1e6d007b940ac6d')
+    // await task.populate('owner').execPopulate()
+    // console.log(task.owner)
+
+    const user = await User.findById('5dbcd5fff1e6d007b940ac6b')
+    await user.populate('tasks').execPopulate()
+    console.log(user.tasks)
+}
+
+main()
--- nodejs/task-manager/src/models/task.js
+++ nodejs/task-manager/src/models/task.js
@@ -1,6 +1,4 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
-
 
 const Task = mongoose.model('Task', {
     description: {
@@ -12,6 +10,11 @@ const Task = mongoose.model('Task', {
     completed: {
         type: Boolean,
         default: false,
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
     }
 })
 
