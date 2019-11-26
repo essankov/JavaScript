eed041598b6be5c2ea0af817a7f8eb4e637aa717
Essa <essank99@gmail.com>
2019-11-25 19:13:49 -0600

    Auto-Cropping and Image Formatting

--- nodejs/task-manager/src/routers/user.js
+++ nodejs/task-manager/src/routers/user.js
@@ -1,8 +1,9 @@
 const express = require('express')
+const multer = require('multer')
+const sharp = require('sharp')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
 const router = new express.Router()
-const multer = require('multer')
 
 // signup 
 router.post('/users', async (req, res) => {
@@ -98,7 +99,8 @@ const upload = multer({
 })
 
 router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
-    req.user.avatar = req.file.buffer
+    const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer()
+    req.user.avatar = buffer
     await req.user.save()
     res.send()
 }, (error, req, res, next) => {
@@ -119,8 +121,8 @@ router.get('/users/:id/avatar', async (req, res) => {
             throw new Error()
         }
 
-        res.set('Content-Type', 'image/jpg')
-        res.send(user.avatar)
+        res.set('Content-Type', 'image/png')
+        res.send (user.avatar)
 
     } catch (e) {
         res.status(404).send()
