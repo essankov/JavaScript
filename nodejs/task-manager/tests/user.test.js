67bd7b2f9d94478300933ccc40d69ccc61c799b5
Essa <essank99@gmail.com>
2019-12-01 18:45:19 -0600

    testing login

---
+++ nodejs/task-manager/tests/user.test.js
@@ -0,0 +1,36 @@
+const request = require('supertest')
+const app = require('../src/app')
+const User = require('../src/models/user')
+
+const userOne = {
+    name: 'Mike',
+    email: 'mike@examole.com',
+    password: '195What!!!'
+}
+
+beforeEach(async () => {
+    await User.deleteMany()
+    await new User(userOne).save()
+})
+
+test('Should signup a new user', async () => {
+    await request(app).post('/users').send({
+        name: 'Essa',
+        email: 'essa@example.com',
+        password: 'MyPass007!'
+    }).expect(201)
+})
+
+test('Should login existing user', async () => {
+    await request(app).post('/users/login').send({
+        email: userOne.email,
+        password: userOne.password
+    }).expect(200)
+})
+
+test('Should not login nonexisting user', async () => {
+    await request(app).post('/users/login').send({
+        email: 'test@example.com',
+        password: 'itsJustAtest'
+    }).expect(400)
+})
