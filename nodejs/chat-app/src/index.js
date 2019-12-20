7d275bd2d9620a6cb8ea6244723e590cd4480427
Essa <essank99@gmail.com>
2019-12-20 15:09:16 -0600

    adding more code for time

--- nodejs/chat-app/src/index.js
+++ nodejs/chat-app/src/index.js
@@ -3,6 +3,7 @@ const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
 const Filter = require('bad-words')
+const { generateMessage } = require('./utils/messages')
 
 const app = express()
 const server = http.createServer(app)
@@ -22,9 +23,9 @@ let count = 0
 io.on('connection', (socket) => {
     console.log('New webSocket connection')
 
+        socket.emit('message', generateMessage('Welcome!'))
     
-    socket.emit('message', 'Welcome!')
-    socket.broadcast.emit('message', 'A new user has joined!')
+    socket.broadcast.emit('message', generateMessage('A new user has joined!'))
 
     socket.on('sendMessage', (message, callback) => {
         const filter = new Filter()
@@ -33,7 +34,7 @@ io.on('connection', (socket) => {
             return callback('Profanity is not allowed!')
         }
 
-        io.emit('message', message)
+        io.emit('message', generateMessage(message))
         callback('')
     })
 
@@ -44,7 +45,7 @@ io.on('connection', (socket) => {
     
 
     socket.on('disconnect', () => {
-        io.emit('message', 'A user has left!')
+        io.emit('message', generateMessage('A user has left!'))
     })
 
 })
