7814ac03958372d45ef3a4c03dae9d1382e3546b
Essa <essank99@gmail.com>
2019-12-20 14:14:37 -0600

    Rendering Location Messages

--- nodejs/chat-app/src/index.js
+++ nodejs/chat-app/src/index.js
@@ -38,9 +38,10 @@ io.on('connection', (socket) => {
     })
 
     socket.on('sendLocation', (coords, callback) => {
-        io.emit('message', `https://www.google.com/maps?q=${coords.latitude},${coords.longitude }`)
+        io.emit('locationMessage', `https://www.google.com/maps?q=${coords.latitude},${coords.longitude }`)
         callback()
     })
+    
 
     socket.on('disconnect', () => {
         io.emit('message', 'A user has left!')
